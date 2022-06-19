93187047053a00fc200a885e3555947e52699fbc
Agu Christopher Chijikem <83035789+dev-kay1@users.noreply.github.com>
2022-06-19 17:50:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,91 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Agu C. Christopher</title>
+  <title>Bootstrap Example</title>
   <meta charset="utf-8">
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-Hi
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<nav class= "navbar navbar-expand-lg navbar-dark bg-dark" >
-<a class= "navbar-brand" href= "#" > Agu C. Christopher</a>
-<button class= "navbar-toggler" type= "button" data-toggle= "collapse" data-target= "#navbarSupportedContent" aria-controls= "navbarSupportedContent" aria-expanded= "false" aria-label= "Toggle navigation" >
-<span class= "navbar-toggler-icon" ></span>
-</button>
-<div class= "collapse navbar-collapse" id= "navbarSupportedContent" >
-<ul class= "navbar-nav mr-auto" >
-<li class= "nav-item active" >
-<a class= "nav-link" href= "#" > Home <span class= "sr-only" > (current) </span></a>
-</li>
-<li class= "nav-item" >
-<a class= "nav-link" href= "#" > Link</a>
-</li>
-<li class= "nav-item dropdown" >
-<a class= "nav-link dropdown-toggle" href= "#" id= "navbarDropdown" role= "button" data-toggle= "dropdown" aria-haspopup= "true" aria-expanded= "false" >
-Dropdown
-</a>
-<div class= "dropdown-menu" aria-labelledby= "navbarDropdown" >
-<a class= "dropdown-item" href= "#" > Action </a>
-<a class= "dropdown-item" href= "#" > Another action </a>
-<div class= "dropdown-divider" ></div>
-<a class= "dropdown-item" href= "#" > Something else here </a>
-</div>
-</li>
-<li class= "nav-item" >
-<a class= "nav-link disabled" href= "#" > Disabled </a>
-</li>
-</ul>
-<form class= "form-inline my-2 my-lg-0" >
-<input class= "form-control mr-sm-2" type= "search" placeholder= "Search" aria-label= "Search" >
-<button class= "btn btn-outline-success my-2 my-sm-0" type= "submit" > Search </button>
-</form>
-</div>
-</nav>
+
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Page 1</a></li>
-        <li><a href="#">Page 2</a></li>
-        <li><a href="#">Page 3</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container bg-dark">
-  <!-- Carousel -->
-<hr />
-<form action="" class="form-group"method="get" accept-charset="utf-8">
-  <label class=""for="">Full Name</label>
-  <input type="text" class="form-control" name="fname" id="fname" value="" />
-  
-  <br />
-<button class="btn btn-success" type="submit"> Send</button>
-<br />
-</form>
-<div class="nt-5 nb-5">
-<div style="width:70%"class="progress">
-<div class="progress-bar progress-bar-striped progress-bar-animated" aria-valuemin="10" aria-valuenow="70" aria-valuemax="100">
-</div>
-</div>
-</div>
-</div>
-</body>
-</html>
