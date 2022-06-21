8e64add526c4cd2224f3241cf03429ac066a0951
Agu Christopher Chijikem <83035789+dev-kay1@users.noreply.github.com>
2022-06-21 07:49:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
   <title>Agu C. Christopher</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/5.0/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.0/js/bootstrap.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
   <script src="js/jquery.js" type="text/javascript" charset="utf-8"></script>
