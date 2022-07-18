938c20f556cb75da52852e27f79ade1ef976e8d6
Agu C. Christopher <83035789+dev-kay1@users.noreply.github.com>
2022-07-18 09:23:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7789232118443297"
+     crossorigin="anonymous"></script>
 <?php 
 include("header.php");
 ?>
